7c395e466df9ed0d6e66c8cbe14f5195fdd6dca9
duyphuc174 <nguyenduyphuc.174@gmail.com>
2023-05-07 23:24:55 +0700

    thêm quái

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -12,6 +12,13 @@ public class Enemy : MonoBehaviour
     public int exp = 10;
     public int score = 10;
 
+    // shoot
+    public bool isShootable = false;
+    public GameObject enemyBullet;
+    public float bulletSpeed = 4f;
+    public float timeBtwFire = 1.5f;
+    private float fireCooldown;
+
 	public AudioClip destroySound;
     AudioSource audioSource;
 
@@ -28,7 +35,26 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        fireCooldown -= Time.deltaTime;
+        if(fireCooldown < 0)
+        {
+            fireCooldown = timeBtwFire;
+            // Shoot
+            if(isShootable)
+            {
+                EnemyFireBullet();
+            }
+        }
+    }
+
+    void EnemyFireBullet()
+    {
+        GameObject bulletTmp = Instantiate(enemyBullet, transform.position, Quaternion.identity);
+        Rigidbody2D rb = bulletTmp.GetComponent<Rigidbody2D>();
+		Vector3 tankPos = FindObjectOfType<Tank>().transform.position;
+		Vector3 direction = tankPos - transform.position;
+        rb.AddForce(direction.normalized * bulletSpeed, ForceMode2D.Impulse);
+
     }
 
     public void TakeDamage(int _damage)
@@ -44,6 +70,7 @@ public class Enemy : MonoBehaviour
     {
         Destroy(this.gameObject);
         gameController.IncreaseScore(score);
+        gameController.DieEnemy();
 		Instantiate(genericEffect, transform.position, Quaternion.identity);
         tank.IncreaseExp(exp);
         if(audioSource && destroySound)
